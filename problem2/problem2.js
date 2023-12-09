ef8099061e77be5a189c46b48c9044cc194fb3e3
vishal Karale <vishalkaralevsk@gmail.com>
2023-12-09 15:12:42 +0530

    problem2 done

--- problem2/problem2.js
+++ problem2/problem2.js
@@ -47,4 +47,22 @@
 // console.log(result);
 
 //  3rd time
+let array = [-5, -4, -3, -2, 0, 2, 4, 6, 8];
+function getSumOfNum (array) {
+    let left = 0;
+    let right = array.length - 1;
 
+    while (left < right) {
+        sum = array[left] + array[right];
+        if (sum === 0) {
+            return ([array[left], array[right]]);
+        } else if (sum > 0) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+}
+
+const result = getSumOfNum(array);
+console.log(result);
