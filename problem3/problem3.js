b257a91b453c8416b8fc56966e0e7d52a9b516fc
vishal Karale <vishalkaralevsk@gmail.com>
2024-02-22 18:47:23 +0530

    push

---
+++ problem3/problem3.js
@@ -0,0 +1,7 @@
+//  string anagram
+//  problem statements Anagram
+//  'hello' and 'ehllo' -  this is anagram because both are equal in length and both have same type of characters
+
+// 1 - check the length of the string if it is bigger then return false
+// 2 - check how many types the letter have
+//  {h:1, e:1,l:2, o:1} => hello 
